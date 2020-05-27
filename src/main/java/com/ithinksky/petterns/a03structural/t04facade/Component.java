ae5c62992d2c2e4b81dd66e2ad61ad3268fcb612
tengpeng.gao <tengpeng.gao@gmail.com>
2020-05-27 19:09:53 +0800

    update 外观模式 done.

---
+++ src/main/java/com/ithinksky/petterns/a03structural/t04facade/Component.java
@@ -0,0 +1,12 @@
+package com.ithinksky.petterns.a03structural.t04facade;
+
+/**
+ * @author tengpeng.gao
+ */
+public interface Component {
+
+	void start();
+
+	void shutdown();
+
+}
---
+++ src/main/java/com/ithinksky/petterns/a03structural/t04facade/Computer.java
@@ -0,0 +1,33 @@
+package com.ithinksky.petterns.a03structural.t04facade;
+
+/**
+ * @author tengpeng.gao
+ */
+public class Computer implements Component {
+
+	private CPU cpu;
+
+	private Disk disk;
+
+	private Memory memory;
+
+	public Computer() {
+		this.cpu = new CPU();
+		this.disk = new Disk();
+		this.memory = new Memory();
+	}
+
+	@Override
+	public void start() {
+		cpu.start();
+		memory.start();
+		disk.start();
+	}
+
+	@Override
+	public void shutdown() {
+		disk.shutdown();
+		memory.shutdown();
+		cpu.shutdown();
+	}
+}
