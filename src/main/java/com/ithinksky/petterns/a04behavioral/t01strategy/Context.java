de30efbfb54364b6036405068d59b3d679a9a9f7
tengpeng.gao <tengpeng.gao@gmail.com>
2020-05-28 22:17:49 +0800

    update 策略模式 done.

---
+++ src/main/java/com/ithinksky/petterns/a04behavioral/t01strategy/Context.java
@@ -0,0 +1,22 @@
+package com.ithinksky.petterns.a04behavioral.t01strategy;
+
+/**
+ * @author tengpeng.gao
+ */
+public class Context {
+
+    private TravelStrategy travelStrategy;
+
+    public TravelStrategy getTravelStrategy() {
+        return travelStrategy;
+    }
+
+    public void setTravelStrategy(TravelStrategy travelStrategy) {
+        this.travelStrategy = travelStrategy;
+    }
+
+    public void travelMode(){
+        this.travelStrategy.travelMode();
+    }
+
+}
---
+++ src/main/java/com/ithinksky/petterns/a04behavioral/t01strategy/TravelStrategy.java
@@ -0,0 +1,10 @@
+package com.ithinksky.petterns.a04behavioral.t01strategy;
+
+/**
+ * @author tengpeng.gao
+ */
+public interface TravelStrategy {
+
+    void travelMode();
+
+}
