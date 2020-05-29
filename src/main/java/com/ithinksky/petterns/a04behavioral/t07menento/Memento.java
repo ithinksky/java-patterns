391767aa8aca16360269e3d959142bc9934d6ee6
tengpeng.gao <tengpeng.gao@gmail.com>
2020-05-29 09:45:41 +0800

    update 备忘录模式 done.

---
+++ src/main/java/com/ithinksky/petterns/a04behavioral/t07menento/Memento.java
@@ -0,0 +1,21 @@
+package com.ithinksky.petterns.a04behavioral.t07menento;
+
+/**
+ * @author tengpeng.gao
+ */
+public class Memento {
+
+    private String value;
+
+    public Memento(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
