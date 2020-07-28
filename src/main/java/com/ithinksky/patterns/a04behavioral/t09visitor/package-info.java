670b2f34183dc69edc1f2f9c28eec7a20e7a3f9b
tengpeng.gao <tengpeng.gao@gmail.com>
2020-07-28 21:38:23 +0800

    update 访问者模式 中介者模式.

--- src/main/java/com/ithinksky/patterns/a04behavioral/t09visitor/package-info.java
+++ src/main/java/com/ithinksky/patterns/a04behavioral/t09visitor/package-info.java
@@ -1,4 +1,7 @@
 /**
+ * 访问者模式
+ * 案例：工程访问：工程可以接受访问者，访问者可以访问工程
+ *
  * @author tengpeng.gao
  */
 package com.ithinksky.patterns.a04behavioral.t09visitor;
--- src/main/java/com/ithinksky/patterns/a04behavioral/t10mediator/package-info.java
+++ src/main/java/com/ithinksky/patterns/a04behavioral/t10mediator/package-info.java
@@ -1,4 +1,7 @@
 /**
+ * 中介者模式
+ * 案例：房屋中介，与房东、与租客交流
+ *
  * @author tengpeng.gao
  */
 package com.ithinksky.patterns.a04behavioral.t10mediator;
